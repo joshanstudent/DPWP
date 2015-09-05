eef56e0f6802c6629b67a89e794fbcee1dfde41a
joshanstudent <josephhandy@Josephs-Mac-mini.local>
2015-09-05 12:57:30 -0400

    intro to python2

--- intro to python/main.py
+++ intro to python/main.py
@@ -7,4 +7,9 @@ first_name = "Kermit"
 last_name = "De Frog"
 
 response = raw_input("Enter your name")
-print response
+# use "print response" to print single statement
+# print response
+
+# use "print "what ever you wish to say within this quote", response" will be for multiple statement
+# It will print out Hello there along with the name the is enterd
+print "Hello there", response
