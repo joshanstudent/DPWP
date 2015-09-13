2335b7596870478a3172277e4d1ae5d5a35db4df
joshanstudent <josephhandy@Josephs-MacBook-Pro.local>
2015-09-13 12:24:07 -0400

    google app launcher

--- data-object/main.py
+++ data-object/main.py
@@ -1,9 +1,39 @@
-
+#data objects
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+    def get(self):   # these are instances
+
+        luke = Character()
+        luke.name = "Luke Skywalker"
+        luke.profession = "Jedia Knight"
+        luke.age = 26
+        luke.home_planet = "Tattooine"
+
+        leia = Character()
+        leia.name = "Princess Leia"
+        leia.profession = "Princess"
+        leia.age = luke.age
+        leia.home_planet = "Alderan"
+
+        yoda = Character()
+        yoda.name = "Master Yoda"
+        yoda.profession = "Jedi Knight"
+        yoda.age = 762
+        yoda.home_planet = "Dagobah"
+
+
+        chars = [luke, leia, yoda]  #this an array
+        print chars[1].profession
+
+
+class Character(object):
+    def __init__(self):   #construtor function
+        self.name = ""
+        self.profession = ""
+        self.age = 0
+        self.home_planet = ""
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
