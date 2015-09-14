00c2971317bf02d8411d74152c9bc1ac998ed75e
joshanstudent <josephhandy@Josephs-MacBook-Pro.local>
2015-09-13 23:18:30 -0400

    Simple Form

--- simple-login/simple-login/main.py
+++ simple-login/simple-login/main.py
@@ -3,14 +3,28 @@ import webapp2 #use the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self):# this function starts everything. Initializing, Catalyst.
-        self.response.write('Hello world!')
+        page = '''<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Simple Form</title>
+    </head>
+    <body>
+        <form method="GET">
+            <label>Name:</label><input type="text" name="user" />
+            <label>Email:</label><input type="text" name="email" />
+            <input type="submit" value="Submit" />
+        </form>
+    </body>
+</html>
+        '''
+        self.response.write(page)
 
 
     #also a (def additional_functions) will be used with next assignment, this is just a
     #early setup of this function
     def additional_functions(self):
         pass
-       
+
 
 
 #never touch this code, this code is here to make sure that python will work within the web browser
