6513ce7b1e6f8611b739681da73ee961bb49297c
joshanstudent <josephhandy@Josephs-MacBook-Pro.local>
2015-09-14 00:09:09 -0400

    Simple Form

--- simple-login/simple-login/main.py
+++ simple-login/simple-login/main.py
@@ -4,37 +4,30 @@ import webapp2 #use the webapp2 library
 class MainHandler(webapp2.RequestHandler): #declaring a class
     def get(self):# this function starts everything. Initializing, Catalyst.
         page_head = '''<!DOCTYPE HTML>
+
 <html>
     <head>
         <title>Simple Form</title>
     </head>
     <body>'''
-
-       page_body = '''<form method="GET">
-            <label>Name:</label><input type="text" name="user" />
-            <label>Email:</label><input type="text" name="email" />
+    page_body = '''<form method="GET" action="">
+            <label>Name: </label><input type="text" name="user" />
+            <label>Email: </label><input type="text" name="email" />
             <input type="submit" value="Submit" />'''
-    
-       page_close = '''
+
+    page_close = '''
         </form>
     </body>
-</html>
-        '''
+</html>'''
+
         if self.request.GET: # the column mean the same as ==True, just another way to write this code
             #print self.request.GET['user'] # This string is to print out to the console
-
-            #stroes info we got from the form
+            #stores info we got from the form
             user = self.request.GET['user'] # this is to print
             email = self.request.GET['email'] # this is to print
-
-        #self.response.write(page) # This is printing out the information of the form
-
-
-    #also a (def additional_functions) will be used with next assignment, this is just a
-    #early setup of this function
-    def additional_functions(self):
-        pass
-
+            self.response.write(page_head + user + ' ' + email + page_body + page_close)
+        else:
+            self.response.write(page_head + page_body + page_close) # This is printing out the information of the form
 
 
 #never touch this code, this code is here to make sure that python will work within the web browser
