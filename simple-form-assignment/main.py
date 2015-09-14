dfd750e9cdf31f3f0fbcd24a60409188567b99ec
joshanstudent <josephhandy@Josephs-MacBook-Pro.local>
2015-09-14 04:58:01 -0400

    simple form assignment

---
+++ simple-form-assignment/main.py
@@ -0,0 +1,19 @@
+# Simple Form Assignment
+
+import webapp2
+from pages import Page #from package import Class
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        p = Page()
+        self.response.write(p.print_out())
+
+
+
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
