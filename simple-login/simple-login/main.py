6ea2072b97ed4cd518dc89ad04cc3f6282eddca9
joshanstudent <josephhandy@Josephs-MacBook-Pro.local>
2015-09-14 03:06:16 -0400

    Simple Form

--- simple-login/simple-login/main.py
+++ simple-login/simple-login/main.py
@@ -18,15 +18,18 @@ class MainHandler(webapp2.RequestHandler): #declaring a class
 
         page_close = '''
         </form>
-    </body> 
+    </body>
 </html>'''
 
 
 # the column mean the same as == True, just another way to write this code# the column mean the same as == True, just another way to write this code
+
+# To access items (client -> server) use this code self.request.GET[]
+#To print items (server -> client) use this code self.response.write()
         if self.request.GET:
             user = self.request.GET['user'] # this is to print
             email = self.request.GET['email'] # this is to print
-            self.response.write(page_head + user + ' ' + email + page_body + page_close)
+            self.response.write(page_head + user + ' ' + email + page_body + page_close)# if you remove page_body the input name and email will display on the web page without the entry forms.
         else:
             self.response.write(page_head + page_body + page_close) # This is printing out the information of the form
 
